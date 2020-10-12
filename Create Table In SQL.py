de9454fd3b29ffbc97c5226bb8178e372ffe9dda
MyCodeingWorld <72703824+MyCodeingWorld@users.noreply.github.com>
2020-10-12 22:51:00 +0530

    Update Create Table In SQL.py

--- Create Table In SQL.py
+++ Create Table In SQL.py
@@ -9,7 +9,7 @@ sql.commit()
 cursor.execute('use accounts')
 sql.commit()
 
-cursor.execute("create table accounts(name char(50), dob char(10), Phone bigint, aadhaar bigint primary key, father char(50), password char(8), address(200), file(100), Amount int")
+cursor.execute("create table accounts(name char(50), dob char(10), Phone bigint, aadhaar bigint primary key, father char(50), password char(8), address char(200), file char(100), Amount int")
 sql.commit()
 
 sql.close()
